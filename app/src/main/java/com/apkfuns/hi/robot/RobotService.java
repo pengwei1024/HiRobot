7b6c8a3ecdfb1f1db7ef579b4fe179d92ffd8209
pengwei <pengwei1024@gmail.com>
2016-02-03 23:31:33 +0800

    基本实现

--- app/src/main/java/com/apkfuns/hi/robot/RobotService.java
+++ app/src/main/java/com/apkfuns/hi/robot/RobotService.java
@@ -1,7 +1,6 @@
 package com.apkfuns.hi.robot;
 
 import android.accessibilityservice.AccessibilityService;
-import android.annotation.SuppressLint;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
@@ -9,6 +8,8 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by pengwei on 16/2/2.
@@ -21,7 +22,7 @@ public class RobotService extends AccessibilityService {
     // 聊天界面
     private static final String CHAT_CLASS_NAME = "com.baidu.hi.activities.Chat";
     // 红包界面
-    private static final String LUCK_MONEY_CLASS_NAME = "com.baidu.hi.luckymoney.LuckyMoneyActivity";
+    private static final String LUCKY_MONEY_CLASS_NAME = "com.baidu.hi.luckymoney.LuckyMoneyActivity";
 
     // 待领取红包列表
     private List<AccessibilityNodeInfo> nodesToFetch = new ArrayList<>();
@@ -31,18 +32,16 @@ public class RobotService extends AccessibilityService {
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         int eventType = event.getEventType();
-        Log.e(TAG, String.valueOf(eventType));
         switch (eventType) {
             case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
-                Log.e(TAG, String.valueOf(currentIsChatActivity));
                 if (currentIsChatActivity) {
-//                    getPacket();
+                    getPacket();
                 }
                 break;
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
                 String className = event.getClassName().toString();
                 currentIsChatActivity = className.equals(CHAT_CLASS_NAME);
-                if (className.equals(LUCK_MONEY_CLASS_NAME)) {
+                if (className.equals(LUCKY_MONEY_CLASS_NAME)) {
                     openPacket();
                 }
                 break;
@@ -53,17 +52,18 @@ public class RobotService extends AccessibilityService {
 
     /**
      * 打开红包
+     * 1.能抢，点击拆红包并退出页面
+     * 2.完成
+     * 2.1 没抢到，退出页面
+     * 2.2 抢过的，退出页面
      */
-    @SuppressLint("NewApi")
     private void openPacket() {
         final AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
-        for (int i = 0; i < nodeInfo.getChildCount(); i++) {
-            Log.e(TAG, nodeInfo.getChild(i).getClassName().toString());
-        }
         if (nodeInfo != null) {
             // 拆红包
             AccessibilityNodeInfo openLucky = findNodeById(nodeInfo, "com.baidu.hi:id/envelope_open");
             if (openLucky != null) {
+                performGlobalAction(GLOBAL_ACTION_BACK);
                 openLucky.performAction(AccessibilityNodeInfo.ACTION_CLICK);
             } else {
                 // 关闭页面
@@ -77,7 +77,7 @@ public class RobotService extends AccessibilityService {
     }
 
     /**
-     * 结点是否存在
+     * 根据id查找结点，存在返回不存在返回null
      *
      * @param nodeInfo
      * @param viewId
@@ -101,41 +101,45 @@ public class RobotService extends AccessibilityService {
      */
     private void toast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-
     }
 
-    @SuppressLint("NewApi")
     private void getPacket() {
         AccessibilityNodeInfo rootNode = getRootInActiveWindow();
-        rootNode.findAccessibilityNodeInfosByText("");
-        recycle(rootNode);
-    }
-
-    @SuppressLint("NewApi")
-    public void recycle(AccessibilityNodeInfo info) {
-        if (info.getChildCount() == 0) {
-            if (info.getText() != null) {
-                Log.e(TAG, info.getText().toString());
-                if (info.getText().equals("百度红包")) {
-                    info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                    AccessibilityNodeInfo parent = info.getParent();
-                    while (parent != null) {
-                        Log.e(TAG, "parent isClick:" + parent.isClickable());
-                        if (parent.isClickable()) {
-                            parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                            break;
-                        }
-                        parent = parent.getParent();
-                    }
-                }
-            }
-        } else {
-            for (int i = 0; i < info.getChildCount(); i++) {
-                if (info.getChild(i) != null) {
-                    recycle(info.getChild(i));
-                }
+        List<AccessibilityNodeInfo> nodes = rootNode.findAccessibilityNodeInfosByViewId("com.baidu.hi:id/lucky_money_title");
+        for (AccessibilityNodeInfo currentNode : nodes) {
+            String nodeId = getNodeId(currentNode);
+            if (!fetchedIdentifiers.contains(nodeId)) {
+                recycle(currentNode);
+                fetchedIdentifiers.add(nodeId);
             }
         }
+    }
+
+    public void recycle(AccessibilityNodeInfo info) {
+        if (info != null) {
+            info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            AccessibilityNodeInfo parent = info.getParent();
+            while (parent != null) {
+                if (parent.isClickable()) {
+                    parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    break;
+                }
+                parent = parent.getParent();
+            }
+        }
+    }
+
+    /**
+     * 获取结点唯一标识符
+     *
+     * @param node
+     * @return
+     */
+    private String getNodeId(AccessibilityNodeInfo node) {
+        Pattern pattern = Pattern.compile("(?<=@)[0-9|a-z]+(?=;)");
+        Matcher matcher = pattern.matcher(node.toString());
+        matcher.find();
+        return matcher.group(0);
     }
 
     @Override
